cd6960ef2f90c8b583adcbc47948c5347fab9f8e
Randy Kobes (contact brian d foy) <bdfoy@cpan.org>
2009-01-02 05:28:10 +0000

    Import of RKOBES/Pod-Perldocs-0.15 from CPAN.
    gitpan-cpan-distribution: Pod-Perldocs
    gitpan-cpan-version:      0.15
    gitpan-cpan-path:         RKOBES/Pod-Perldocs-0.15.tar.gz
    gitpan-cpan-author:       RKOBES
    gitpan-cpan-maturity:     released

--- lib/Pod/Perldocs.pm
+++ lib/Pod/Perldocs.pm
@@ -2,43 +2,83 @@ package Pod::Perldocs;
 use strict;
 use warnings;
 require Pod::Perldoc;
+use LWP::UserAgent;
 use base qw(Pod::Perldoc);
 our ($VERSION);
-$VERSION = '0.1';
+$VERSION = '0.15';
 
 ################################################################
 # Change the following to reflect your setup
 my $soap_uri = 'http://theoryx5.uwinnipeg.ca/Apache/DocServer';
 my $soap_proxy = 'http://theoryx5.uwinnipeg.ca/cgi-bin/docserver.cgi';
+my $pod_server = q{http://cpan.uwinnipeg.ca/cgi-bin/podserver.cgi};
 ###############################################################
 
 sub grand_search_init {
-    my($self, $pages, @found) = @_;
-    @found = $self->SUPER::grand_search_init($pages, @found);
-    return @found if @found;
-    my $soap = make_soap() or return @found; # no SOAP::Lite available
-    print STDERR "Searching on remote soap server ...\n";
-    my $result = $soap->get_doc($pages->[0]);
-    defined $result && defined $result->result or do {
-        print STDERR "No matches found there either.\n";
-        return @found;
-    };
-    my $lines = $result->result();
-    unless ($lines and ref($lines) eq 'ARRAY') {
-        print STDERR "Documentation not found there either.\n";
-        return @found;
-    }
-    my ($fh, $filename) = $self->new_tempfile();
-    print $fh @$lines;
+  my($self, $pages, @found) = @_;
+  @found = $self->SUPER::grand_search_init($pages, @found);
+  return @found if @found;
+  print STDERR "Searching on remote pod server ...\n";
+  my $filename;
+  if ($filename = get_lwp($self, $pages->[0])) {
     push @found, $filename;
     return @found;
+  }
+  elsif ($filename = get_soap($pages->[0])) {
+    push @found, $filename;
+    return @found;
+  }
+  else {
+    return @found;
+  }
+}
+
+sub get_lwp {
+  my ($self, $mod) = @_;
+  my $ua = LWP::UserAgent->new;
+  $ua->agent("Pod/Perldocs 0.15 ");
+  push @{ $ua->requests_redirectable }, 'POST';
+  # Create a request
+  my $req = HTTP::Request->new(POST => $pod_server);
+  $req->content_type('application/x-www-form-urlencoded');
+  $req->content("mod=$mod");
+  # Pass request to the user agent and get a response back
+  my $res = $ua->request($req);
+  # Check the outcome of the response
+  if ($res->is_success) {
+    my ($fh, $filename) = $self->new_tempfile();
+    print $fh $res->content;
+    return $filename;
+  }
+  else {
+    print STDERR "Remote server returned status code: " . $res->status_line . "\n";
+    return;
+  }
+}
+
+sub get_soap {
+  my ($self, $mod) = @_;
+  my $soap = make_soap() or return; # no SOAP::Lite available
+  my $result = $soap->get_doc($mod);
+  defined $result && defined $result->result or do {
+    print STDERR "No matches found there either.\n";
+    return;
+  };
+  my $lines = $result->result();
+  unless ($lines and ref($lines) eq 'ARRAY') {
+    print STDERR "Documentation not found there either.\n";
+    return;
+  }
+  my ($fh, $filename) = $self->new_tempfile();
+  print $fh @$lines;
+  return $filename;
 }
 
 sub make_soap {
   unless (eval { require SOAP::Lite }) {
     print STDERR "SOAP::Lite is unavailable to make remote call\n";
     return undef;
-  } 
+  }
 
   return SOAP::Lite
     ->uri($soap_uri)
@@ -64,13 +104,13 @@ Pod::Perldocs - view remote pod via Pod::Perldoc
 This is a drop-in replacement for C<perldoc> based on
 C<Pod::Perldoc>. Usage is the same, except in the case
 when documentation for a module cannot be found on the
-local machine, in which case a query (via SOAP::Lite) will
+local machine, in which case a query (via LWP or SOAP::Lite) will
 be made to a remote pod repository and, if the documentation is
 found there, the results will be displayed as usual.
 
 =head1 NOTE
 
-The values of C<$soap_uri> and
+The values of C<$pod_server>, C<$soap_uri> and
 C<$soap_proxy> at the top of this script reflect
 the location of the remote pod repository.
 
@@ -87,7 +127,7 @@ L<Pod::Perldoc>.
 
 =head1 COPYRIGHT
 
-This software is copyright 2004 by Randy Kobes
+This software is copyright 2004,2009 by Randy Kobes
 E<lt>r.kobes@uwinnipeg.caE<gt>. Usage and redistribution
 is under the same terms as Perl itself.
 
